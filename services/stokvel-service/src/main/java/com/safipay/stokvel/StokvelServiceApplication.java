5b731eafea8fdb908384a9c8e55a45916088ddf2
RethaMokoena <162606472+RethaMokoena@users.noreply.github.com>
2025-12-03 00:30:53 +0200

    project intialization

---
+++ services/stokvel-service/src/main/java/com/safipay/stokvel/StokvelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.safipay.stokvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StokvelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StokvelServiceApplication.class, args);
+	}
+
+}
---
+++ services/stokvel-service/src/test/java/com/safipay/stokvel/StokvelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safipay.stokvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StokvelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
